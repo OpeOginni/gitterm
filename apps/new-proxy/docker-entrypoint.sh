2eb824ea45c19e0ffb0563abec2e36b7853db415
OpeOginni <brightoginni123@gmail.com>
2025-12-09 13:21:32 +0100

    refactor(proxy): remove custom entrypoint script and leverage nginx's built-in template processing for configuration

--- apps/new-proxy/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-envsubst '${INTERNAL_API_KEY}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-rm /etc/nginx/conf.d/default.conf.template
-
-exec "$@"
